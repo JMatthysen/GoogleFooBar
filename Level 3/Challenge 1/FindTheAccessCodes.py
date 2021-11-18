3d009fe8868b0ff9a5d8dc25fb1c5ed1ecb04f03
Jonathan Matthysen <57965951+JMatthysen@users.noreply.github.com>
2021-11-17 19:49:59 -0600

    NightStuff

--- Level 3/Challenge 1/FindTheAccessCodes.py
+++ Level 3/Challenge 1/FindTheAccessCodes.py
@@ -3,22 +3,31 @@ import random
 
 
 def solution1(l):
-    unique_numbers = {}
+    class Node:
+        def __init__(self, val):
+            self.val = val
+            self.children = []
 
-    trees = {}
+        def print_vals(self):
+            print([child.val for child in self.children])
+
+    tree = {}
+
     for num in l:
-        if num not in trees.keys():
-            trees[num] = []
-
-        for key in trees:
+        temp_node = Node(num)
+        for key in tree.keys():
             if num % key == 0:
-                trees[key].append(num)
-    print(trees)
+                for node in tree[key]:
+                    node.children.append(temp_node)
+
+        if num not in tree.keys():
+            tree[num] = [temp_node]
+
+    
 
     return 0
 
 def generate_test_case(nums, low=1, high=999999):
-    nums = []
     return [random.randint(low, high) for i in range(nums)]
 
 
