0dfd2780c60ee4d73bc5794a45c635a6895b070d
JMatthysen <jmatthysen@benoit-inc.com>
2021-11-24 16:40:28 -0600

    still struggling

--- Level 3/Challenge 2/TheGrandestStaircaseOfThemAll.py
+++ Level 3/Challenge 2/TheGrandestStaircaseOfThemAll.py
@@ -1,19 +1,63 @@
 import Tools
 
+
 def solution1(n):
     class Bar:
+        bar_id = 0
+
         def __init__(self, val):
             self.val = val
+            self.id  = Bar.bar_id
+            Bar.bar_id += 1
 
-        def _split(self, ):
+        def look_for_offspring(self,):
+            child_size = 0
+            last_child_size = child_size
+            still_looking = True
+            perm_count, child_perms = 0, 0
+            while still_looking:
+                if  self.val == 1:
+                    return perm_count + child_perms
+                elif self.val <= child_size:
+                    break
 
-    count = 0
-    brick_pool = n
-    still_looking = True
-    while still_looking:
-        brick_pool = n
-        bars = []
-        first_bar = brick_pool - 1
+                child_size = last_child_size + 1
+                self.val  -= 1
+                print( self.id, self.val, child_size, child_perms)
+                child_bar   = Bar(child_size)
+                child_perms += child_bar.look_for_offspring()
+                perm_count += 1
+                last_child_size = child_size
+                print( child_perms)
+            return perm_count + child_perms
+
+    def find_sums(num):
+        sum_list = []
+        while True:
+            pass
+
+
+    sum_vars = []
+    for i in range(10):
+        pass
+
+    base_bar = Bar(n)
+    count    = base_bar.look_for_offspring()
+    print("Final count ", count)
+
+
+def solution2(n):
+    def get_addend_pairs(num):
+        numbers = [n for n in range(num, -1, -1)]
+
+
+
+
+    for i in range(n, 0, -1):
+        remainder = n - i
+        remainder_addends = [j for j in range(remainder, -1, -1)]
+        print(i, remainder, remainder_addends)
+
 
 
 
@@ -24,8 +68,10 @@ def test_solution():
                   [5, 2]
 
                   ]
-
-    Tools.run_tests(solution1, test_cases, True, 1)
+    solution2(3)
+    print()
+    solution2(4)
+    #Tools.run_tests(solution1, test_cases, True, 1)
     #Tools.run_tests(solution2, test_cases, False, 10000)
     #Tools.run_tests(solution3, test_cases, False, 10000)
     #Tools.run_tests(solution4, test_cases, False, 10000)
